73444ebd1c3dde494a6b27b1d1528d25c58a103e
merloxx <hello@merloxx.it>
2023-08-07 17:22:03 +0200

    New: Added @throw tag to RouteCollectorInterface doc blocs

--- src/Contracts/RouteCollectorInterface.php
+++ src/Contracts/RouteCollectorInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Zaphyr\Route\Contracts;
 
 use Zaphyr\Route\Attributes\Route;
+use Zaphyr\Route\Exceptions\RouteException;
 
 /**
  * @author merloxx <merloxx@zaphyr.org>
@@ -16,6 +17,7 @@ interface RouteCollectorInterface
      * @param string[]                                     $methods
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function add(string $path, array $methods, array|callable|string $callable): Route;
@@ -24,6 +26,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function any(string $path, array|callable|string $callable): Route;
@@ -32,6 +35,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function get(string $path, array|callable|string $callable): Route;
@@ -40,6 +44,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function post(string $path, array|callable|string $callable): Route;
@@ -48,6 +53,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function put(string $path, array|callable|string $callable): Route;
@@ -56,6 +62,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function patch(string $path, array|callable|string $callable): Route;
@@ -64,6 +71,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function delete(string $path, array|callable|string $callable): Route;
@@ -72,6 +80,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function head(string $path, array|callable|string $callable): Route;
@@ -80,6 +89,7 @@ interface RouteCollectorInterface
      * @param string                                       $path
      * @param callable|string|array<string|object, string> $callable
      *
+     * @throws RouteException
      * @return Route
      */
     public function options(string $path, array|callable|string $callable): Route;
